11f016501d4871452aa8da2138a37539a63160e3
Paras Choudhary <55029829+paras9@users.noreply.github.com>
2024-05-14 15:21:13 +0530

    six

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
    <div className="flex flex-col gap-y-4">
     <h1>
-      DashBoard
+      Home page
     </h1>
   <UserButton afterSignOutUrl="/"
   />
