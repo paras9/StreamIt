24c0e9c3038401547ae47be6d267438dab0687c2
Paras Choudhary <55029829+paras9@users.noreply.github.com>
2024-05-14 10:25:31 +0530

    third

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <p>hi</p>
-  );
+   <div className="flex flex-col gap-y-4">
+    <h1>
+      DashBoard
+    </h1>
+  <UserButton afterSignOutUrl="/"
+  />
+   </div> 
+  )
 }
