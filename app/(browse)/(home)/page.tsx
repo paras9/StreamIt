9d16bbbe9510f85086c5cd6f71892a1e2e439079
Paras Choudhary <55029829+paras9@users.noreply.github.com>
2024-05-17 01:30:59 +0530

    22A

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,13 +1,14 @@
 import { UserButton } from "@clerk/nextjs";
 import { Suspense } from "react";
+import { Results, ResultsSkeleton } from "./_components/results";
 
 export default function Home() {
   return (
     <div className="h-full p-8 max-w-screen-2xl mx-auto">
-    {/*<Suspense fallback={<ResultsSkeleto />}>
+    <Suspense fallback={<ResultsSkeleton />}>
       <Results />
-  </Suspense>*/}
-  <h1>Home Page</h1>
+  </Suspense>
+ 
   </div> 
   )
 }
