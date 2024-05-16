03d7e8db4043f9cfd1272707159aeffde8271ce3
Paras Choudhary <55029829+paras9@users.noreply.github.com>
2024-05-16 21:02:30 +0530

    18A

---
+++ components/stream-player/chat-community.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+interface ChatCommunityProps {
+    hostName: string;
+    viewerName: string;
+    isHidden: boolean;
+};
+
+export const ChatCommunity =({}: ChatCommunityProps) =>{
+    return(
+        <div className=""></div>
+    )
+}
--- components/stream-player/chat-list.tsx
+++ components/stream-player/chat-list.tsx
@@ -2,6 +2,7 @@
 
 import { ReceivedChatMessage } from "@livekit/components-react";
 import { ChatMessage } from "./Chat-message";
+import { Skeleton } from "../ui/skeleton";
 
 interface ChatListProps {
 	messages: ReceivedChatMessage[];
@@ -30,5 +31,12 @@ export const ChatList = ({
             />
         ))}
     </div>
-    )
-}
+    );
+};
+export const ChatListSkeleton = () => {
+	return (
+		<div className="flex h-full items-center justify-center">
+			<Skeleton className="w-1/2 h-6" />
+		</div>
+	);
+};
